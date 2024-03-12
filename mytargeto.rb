98672d47b49626b6289dcc3ba9490ede20276200
suman <yl@logicode.in>
2024-03-12 17:16:37 +0530

    first commit

---
+++ mytargeto.rb
@@ -0,0 +1,29 @@
+class testscript < Formula
+ desc ""
+ homepage ""
+ url "https://github.com/MakwanaSuman/homebrewformula/archive/refs/tags/v1.0.0.tar.gz"
+
+def install 
+ bin.install "mytargeto"
+
+end
+
+end
+
+
+
+
+
+
+:qa
+
+:WQ
+
+
+
+
+
+i
+
+
+
